950e4f3c79feac587a7695366a5798bb38b530e7
Amey Thakur <ameythakur20@gmail.com>
2026-01-02 18:09:39 -0500

    Car Rental Database Management System

--- car/admin/includes/leftbar.php
+++ car/admin/includes/leftbar.php
@@ -13,14 +13,14 @@
 
 <li><a href="#"><i class="fa fa-sitemap"></i> Vehicles</a>
 					<ul>
-						<li><a href="post-avehical.php">Post a Vehicle</a></li>
+						<li><a href="post-avehicle.php">Post a Vehicle</a></li>
 						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
 					</ul>
 				</li>
 				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
 
 				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
-				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop""></i> Manage Conatctus Query</a></li>
+				<li><a href="manage-contactusquery.php"><i class="fa fa-desktop""></i> Manage Contactus Query</a></li>
 				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
 			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
 			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
