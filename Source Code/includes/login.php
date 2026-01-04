2f8a8346d635ad8e15588bf26f8d135a4fbe72d2
Amey Thakur <ameythakur20@gmail.com>
2026-01-04 17:37:35 -0500

    Car Rental Database Management System

--- Source Code/includes/login.php
+++ Source Code/includes/login.php
@@ -1,34 +1,73 @@
 <?php
 /**
- * Car Rental Database Management System
+ * ============================================================================
+ * Car Rental Database Management System - User Authentication
+ * ============================================================================
  * 
- * @author      Amey Thakur
+ * This file handles the user login process. It uses a modal interface for
+ * credential submission and performs server-side validation against the
+ * database. Successful login initializes session variables for the user.
+ * 
+ * ----------------------------------------------------------------------------
+ * AUTHORSHIP & CREDITS (AHNA Team)
+ * ----------------------------------------------------------------------------
+ * This project was developed by the AHNA team:
+ * - Amey Thakur
+ * - Hasan Rizvi
+ * - Nithya Gnanasekar
+ * - Anisha Gupta
+ * 
+ * @package     CarRentalSystem
+ * @subpackage  Authentication
+ * @author      Amey Thakur (Lead)
  * @link        https://github.com/Amey-Thakur
  * @repository  https://github.com/Amey-Thakur/CAR-RENTAL-SYSTEM
+ * @version     1.0.0
  * @date        2021-01-19
  * @license     MIT
+ * 
+ * ============================================================================
+ * CHANGE LOG:
+ * ----------------------------------------------------------------------------
+ * 2021-01-19 - Initial release - AHNA Team
+ * ============================================================================
  */
 
+/**
+ * Login Handler
+ * 
+ * Processes the login form submission.
+ * Hashes the submitted password using MD5 and compares it with the stored hash
+ * in the 'tblusers' table.
+ */
 if (isset($_POST['login'])) {
   $email = $_POST['email'];
   $password = md5($_POST['password']);
+
+  // Credentials Verification Query
   $sql = "SELECT EmailId,Password,FullName FROM tblusers WHERE EmailId=:email and Password=:password";
   $query = $dbh->prepare($sql);
   $query->bindParam(':email', $email, PDO::PARAM_STR);
   $query->bindParam(':password', $password, PDO::PARAM_STR);
   $query->execute();
   $results = $query->fetchAll(PDO::FETCH_OBJ);
+
   if ($query->rowCount() > 0) {
+    // Authentication Successful: Initialize Session
     $_SESSION['login'] = $_POST['email'];
     $_SESSION['fname'] = $results->FullName;
+
+    // Refresh Page to Apply Login State
     $currentpage = $_SERVER['REQUEST_URI'];
     echo "<script type='text/javascript'> document.location = '$currentpage'; </script>";
   } else {
+    // Authentication Failed
     echo "<script>alert('Invalid Details');</script>";
   }
 }
 ?>
 
+<!-- Login Modal -->
 <div class="modal fade" id="loginform">
   <div class="modal-dialog" role="document">
     <div class="modal-content" style="background-color:aqua;">
@@ -41,6 +80,8 @@ if (isset($_POST['login'])) {
         <div class="row">
           <div class="login_wrap">
             <div class="col-md-12 col-sm-6">
+
+              <!-- Login Form -->
               <form method="post">
                 <div class="form-group">
                   <input type="email" class="form-control" name="email" placeholder="Enter Email Address">
@@ -49,6 +90,7 @@ if (isset($_POST['login'])) {
                   <input type="password" class="form-control" name="password" placeholder="Password">
                 </div>
                 <div class="form-group checkbox">
+                  <!-- 'Remember Me' Checkbox (Preserved from legacy code, functionality pending) -->
                   <input type="checkbox" id="remember">
                 </div>
                 <div class="form-group">
@@ -56,18 +98,24 @@ if (isset($_POST['login'])) {
                     style="background-color:blue;">
                 </div>
               </form>
+
             </div>
           </div>
         </div>
       </div>
+
+      <!-- Modal Footer / Links -->
       <div>
-        <p><a href="#forgotpassword" data-toggle="modal" data-dismiss="modal"
-            style="color:blue;">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspForgot Password...?</a>&nbsp &nbsp
+        <p>
+          <a href="#forgotpassword" data-toggle="modal" data-dismiss="modal"
+            style="color:blue;">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspForgot Password...?</a>
+          &nbsp &nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp &nbsp
           &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp &nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-          &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp &nbsp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-          &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <a
-            href="#signupform" data-toggle="modal" data-dismiss="modal" style="color:blue;">Make My Account...</a> </p>
+          &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+          <a href="#signupform" data-toggle="modal" data-dismiss="modal" style="color:blue;">Make My Account...</a>
+        </p>
       </div>
+
     </div>
   </div>
 </div>
