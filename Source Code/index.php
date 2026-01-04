7271f8283333247bdcc3ffe6b1ca5bc6ffd153d2
Amey Thakur <ameythakur20@gmail.com>
2026-01-04 17:27:44 -0500

    Car Rental Database Management System

--- Source Code/index.php
+++ Source Code/index.php
@@ -8,9 +8,18 @@
  * It displays the homepage with marketing content, service highlights,
  * platform statistics, and provides access to user authentication modals.
  * 
+ * ----------------------------------------------------------------------------
+ * AUTHORSHIP & CREDITS (AHNA Team)
+ * ----------------------------------------------------------------------------
+ * This project was developed by the AHNA team:
+ * - Amey Thakur
+ * - Hasan Rizvi
+ * - Nithya Gnanasekar
+ * - Anisha Gupta
+ * 
  * @package     CarRentalSystem
  * @subpackage  Frontend
- * @author      Amey Thakur
+ * @author      Amey Thakur (Lead)
  * @link        https://github.com/Amey-Thakur
  * @repository  https://github.com/Amey-Thakur/CAR-RENTAL-SYSTEM
  * @version     1.0.0
@@ -25,7 +34,7 @@
  * ============================================================================
  * CHANGE LOG:
  * ----------------------------------------------------------------------------
- * 2021-01-19 - Initial release - Amey Thakur
+ * 2021-01-19 - Initial release - AHNA Team
  * ============================================================================
  */
 
@@ -81,10 +90,10 @@ The page is divided into the following main sections:
 <head>
   <!-- Character Encoding: UTF-8 for international character support -->
   <meta charset="UTF-8">
-  
+
   <!-- Page Title: Displayed in browser tab and search results -->
   <title>AHNA | CAR Rental</title>
-  
+
   <!-- 
   ============================================================================
   CSS DEPENDENCIES
@@ -98,26 +107,26 @@ The page is divided into the following main sections:
   6. Google Fonts - Custom typography (Play font family)
   ============================================================================
   -->
-  
+
   <!-- Bootstrap Framework: Responsive grid and core components -->
   <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
-  
+
   <!-- Custom Application Styles: Brand-specific theming -->
   <link rel="stylesheet" href="assets/css/style.css" type="text/css">
-  
+
   <!-- Owl Carousel: Touch-enabled slider component -->
   <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
   <link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
-  
+
   <!-- Slick Slider: Alternative carousel with advanced features -->
   <link href="assets/css/slick.css" rel="stylesheet">
-  
+
   <!-- Bootstrap Slider: Range input component -->
   <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
-  
+
   <!-- Font Awesome: Scalable vector icons -->
   <link href="assets/css/font-awesome.min.css" rel="stylesheet">
-  
+
   <!-- Google Fonts: Play font for modern typography -->
   <link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet">
 
@@ -159,7 +168,7 @@ The page is divided into the following main sections:
 </head>
 
 <body style="background-color:black;">
-  
+
   <!--
   ============================================================================
   HEADER SECTION
@@ -186,13 +195,13 @@ The page is divided into the following main sections:
   <section id="banner">
     <!-- Hero Image: Full-width promotional banner -->
     <img src="admin/img/vehicleimages/banner-image.jpg" style="width:100%;">
-    
+
     <div class="container">
       <!-- Promotional Tagline: COVID-19 safety messaging -->
       <div class="text-block">
         <h4 style="color:white;">DRIVE IN A SANITISED CAR</h4><br /><br />
       </div>
-      
+
       <!-- Primary Call-to-Action: Vehicle search link -->
       <div class="text-blockk">
         <a href="car-listing.php" class="btn" style="background-color:blue;">Search Cars</a><br /><br /><br />
@@ -220,7 +229,7 @@ The page is divided into the following main sections:
         <br />
         <!-- Section Heading -->
         <h2 style="color:aqua;">Self Drive Cars on Rent</h2>
-        
+
         <!-- Taglines -->
         <h3 style="color:white;">
           <span>We simplified car rentals, so you can focus on what's important to you.</span>
@@ -228,40 +237,43 @@ The page is divided into the following main sections:
         <h3 style="color:white;">
           <span>Unbeatable Rates. Easy & Quick Online Booking. Clean & Well Maintained Fleet.</span>
         </h3>
-        
+
         <!-- Feature Cards Grid -->
         <p>
         <div class="items">
           <!-- Feature 1: Fuel Cost Included -->
           <div>
             <h3 style="color:aqua;">Fuel Cost Included</h3>
-            <p style="color:white;">Don't worry about mileage! All fuel costs are included. If you refill fuel, we'll pay you back!</p>
+            <p style="color:white;">Don't worry about mileage! All fuel costs are included. If you refill fuel, we'll
+              pay you back!</p>
           </div>
-          
+
           <!-- Feature 2: No Hidden Charges -->
           <div>
             <h3 style="color:aqua;">No Hidden Charges</h3>
             <p style="color:white;">Our prices include taxes and insurance.<br>What you see is what you really pay!</p>
           </div>
-          
+
           <!-- Feature 3: Flexible Pricing -->
           <div>
             <h3 style="color:aqua;">Flexi Pricing Packages</h3>
-            <p style="color:white;">One size never fits all! Choose a balance of time and kilometers that works best for you.</p>
+            <p style="color:white;">One size never fits all! Choose a balance of time and kilometers that works best for
+              you.</p>
           </div>
-          
+
           <!-- Feature 4: All-India Permits -->
           <div>
             <h3 style="color:aqua;">Go Anywhere</h3>
-            <p style="color:white;">Our cars have all-India permits. Just remember to pay state tolls and entry taxes.</p>
+            <p style="color:white;">Our cars have all-India permits. Just remember to pay state tolls and entry taxes.
+            </p>
           </div>
-          
+
           <!-- Feature 5: 24/7 Support -->
           <div>
             <h3 style="color:aqua;">24x7 Roadside Assistance</h3>
             <p style="color:white;">We have round-the-clock, pan India partners. Help is never far away from you.</p>
           </div>
-          
+
           <!-- Feature 6: Insurance Coverage -->
           <div>
             <h3 style="color:aqua;">Damage Insurance</h3>
@@ -298,7 +310,7 @@ The page is divided into the following main sections:
               </div>
             </div>
           </div>
-          
+
           <!-- Statistic 2: Distance Covered -->
           <div class="col-lg-3 col-xs-6 col-sm-3">
             <div class="fun-facts-m" style="background-color:blue;">
@@ -308,7 +320,7 @@ The page is divided into the following main sections:
               </div>
             </div>
           </div>
-          
+
           <!-- Statistic 3: User Base -->
           <div class="col-lg-3 col-xs-6 col-sm-3">
             <div class="fun-facts-m" style="background-color:blue;">
@@ -318,7 +330,7 @@ The page is divided into the following main sections:
               </div>
             </div>
           </div>
-          
+
           <!-- Statistic 4: Fleet Size -->
           <div class="col-lg-3 col-xs-6 col-sm-3">
             <div class="fun-facts-m" style="background-color:blue;">
@@ -330,11 +342,11 @@ The page is divided into the following main sections:
           </div>
         </div>
       </div>
-      
+
       <!-- Visual overlay for depth effect -->
       <div class="dark-overlay"></div>
     </section>
-    
+
     <!--
     ============================================================================
     FOOTER SECTION
@@ -363,7 +375,7 @@ The page is divided into the following main sections:
     <?php include('includes/login.php'); ?>
     <?php include('includes/registration.php'); ?>
     <?php include('includes/forgotpassword.php'); ?>
-    
+
     <!--
     ============================================================================
     JAVASCRIPT DEPENDENCIES
@@ -376,22 +388,22 @@ The page is divided into the following main sections:
     4. Slider plugins - Carousel functionality
     ============================================================================
     -->
-    
+
     <!-- jQuery: Core JavaScript library -->
     <script src="assets/js/jquery.min.js"></script>
-    
+
     <!-- Bootstrap: UI component interactions -->
     <script src="assets/js/bootstrap.min.js"></script>
-    
+
     <!-- Interface: Custom application logic -->
     <script src="assets/js/interface.js"></script>
-    
+
     <!-- Bootstrap Slider: Range input functionality -->
     <script src="assets/js/bootstrap-slider.min.js"></script>
-    
+
     <!-- Slick Slider: Carousel implementation -->
     <script src="assets/js/slick.min.js"></script>
-    
+
     <!-- Owl Carousel: Alternative carousel library -->
     <script src="assets/js/owl.carousel.min.js"></script>
 
