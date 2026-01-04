215e16a1b9b7f7795fade4c1ae6ee5141bb2a9ee
Amey Thakur <ameythakur20@gmail.com>
2026-01-04 17:57:22 -0500

    Car Rental Database Management System

--- Source Code/admin/includes/leftbar.php
+++ Source Code/admin/includes/leftbar.php
@@ -1,12 +1,43 @@
 <!--
- * Car Rental Database Management System - Admin Module
- * 
- * @author      Amey Thakur
- * @link        https://github.com/Amey-Thakur
- * @repository  https://github.com/Amey-Thakur/CAR-RENTAL-SYSTEM
- * @date        2021-01-19
- * @license     MIT
+============================================================================
+Car Rental Database Management System - Admin Sidebar Navigation
+============================================================================
+
+This file contains the HTML markup for the admin panel's sidebar navigation.
+It provides links to all major administrative functions including:
+- Dashboard
+- Brand Management
+- Vehicle Management
+- Booking Management
+- Content Management (Testimonials, Contact Queries, Pages)
+- User Management
+- Subscriber Management
+
+----------------------------------------------------------------------------
+AUTHORSHIP & CREDITS (AHNA Team)
+----------------------------------------------------------------------------
+This project was developed by the AHNA team:
+- Amey Thakur
+- Hasan Rizvi
+- Nithya Gnanasekar
+- Anisha Gupta
+
+@package     CarRentalSystem
+@subpackage  Admin
+@author      Amey Thakur (Lead)
+@link        https://github.com/Amey-Thakur
+@repository  https://github.com/Amey-Thakur/CAR-RENTAL-SYSTEM
+@version     1.0.0
+@date        2021-01-19
+@license     MIT
+
+============================================================================
+CHANGE LOG:
+----------------------------------------------------------------------------
+2021-01-19 - Initial release - AHNA Team
+============================================================================
 -->
+
 <nav class="ts-sidebar">
 	<ul class="ts-sidebar-menu">
 
@@ -29,8 +60,8 @@
 		<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
 
 		<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
-		<li><a href="manage-contactusquery.php"><i class="fa fa-desktop""></i> Manage Contactus Query</a></li>
-				<li><a href=" reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+		<li><a href="manage-contactusquery.php"><i class="fa fa-desktop"></i> Manage Contactus Query</a></li>
+		<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
 		<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
 		<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
 
