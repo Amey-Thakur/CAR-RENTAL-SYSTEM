39ebad0289c6d608c752e74db4b2f082b21d2e00
Amey Thakur <ameythakur20@gmail.com>
2026-01-04 17:34:01 -0500

    Car Rental Database Management System

--- Source Code/update-password.php
+++ Source Code/update-password.php
@@ -1,37 +1,83 @@
-<!--
- * Car Rental Database Management System
+<?php
+/**
+ * ============================================================================
+ * Car Rental Database Management System - Password Management
+ * ============================================================================
  * 
- * @author      Amey Thakur
+ * This file allows users to securely update their account password.
+ * It includes client-side validation for password matching and server-side
+ * verification of the current password before applying changes.
+ * 
+ * ----------------------------------------------------------------------------
+ * AUTHORSHIP & CREDITS (AHNA Team)
+ * ----------------------------------------------------------------------------
+ * This project was developed by the AHNA team:
+ * - Amey Thakur
+ * - Hasan Rizvi
+ * - Nithya Gnanasekar
+ * - Anisha Gupta
+ * 
+ * @package     CarRentalSystem
+ * @subpackage  UserDashboard
+ * @author      Amey Thakur (Lead)
  * @link        https://github.com/Amey-Thakur
  * @repository  https://github.com/Amey-Thakur/CAR-RENTAL-SYSTEM
+ * @version     1.0.0
  * @date        2021-01-19
  * @license     MIT
--->
-<style>
-  div {
-    font-family: Play;
-    font-size: 20px;
-    color: BLUE;
-  }
-</style>
+ * 
+ * @requires    PHP 7.0+
+ * @requires    MySQL 5.7+
+ * 
+ * ============================================================================
+ * CHANGE LOG:
+ * ----------------------------------------------------------------------------
+ * 2021-01-19 - Initial release - AHNA Team
+ * ============================================================================
+ */
 
-<?php
+/**
+ * Session Initialization
+ */
 session_start();
+
+/**
+ * Error Reporting
+ */
 error_reporting(0);
+
+/**
+ * Database Connection
+ */
 include('includes/config.php');
+
+/**
+ * Access Control and Password Logic
+ */
 if (strlen($_SESSION['login']) == 0) {
+  // Redirect to login if session is invalid
   header('location:index.php');
 } else {
+
+  /**
+   * Password Update Handler
+   * Processes the form submission for password change.
+   */
   if (isset($_POST['update'])) {
+    // Hash passwords using MD5 (Legacy system requirement)
     $password = md5($_POST['password']);
     $newpassword = md5($_POST['newpassword']);
     $email = $_SESSION['login'];
+
+    // Verify Current Password
     $sql = "SELECT Password FROM tblusers WHERE EmailId=:email and Password=:password";
     $query = $dbh->prepare($sql);
     $query->bindParam(':email', $email, PDO::PARAM_STR);
     $query->bindParam(':password', $password, PDO::PARAM_STR);
     $query->execute();
     $results = $query->fetchAll(PDO::FETCH_OBJ);
+
+    // If match found, update to new password
     if ($query->rowCount() > 0) {
       $con = "update tblusers set Password=:newpassword where EmailId=:email";
       $chngpwd1 = $dbh->prepare($con);
@@ -52,23 +98,19 @@ if (strlen($_SESSION['login']) == 0) {
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="keywords" content="">
-    <meta name="description" content="">
+
     <title>AHNA | CAR Rental</title>
-    <!--Bootstrap -->
+
+    <!-- CSS Dependencies -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
-    <!--Custome Style -->
     <link rel="stylesheet" href="assets/css/style.css" type="text/css">
-    <!--OWL Carousel slider-->
     <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
     <link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
-    <!--slick-slider -->
     <link href="assets/css/slick.css" rel="stylesheet">
-    <!--bootstrap-slider -->
     <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
-    <!--FontAwesome Font Style -->
     <link href="assets/css/font-awesome.min.css" rel="stylesheet">
-    <!-- Fav and touch icons -->
+
+    <!-- Browser Icons -->
     <link rel="apple-touch-icon-precomposed" sizes="144x144"
       href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114"
@@ -77,9 +119,11 @@ if (strlen($_SESSION['login']) == 0) {
       href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
     <link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
-    <!-- Google-Font-->
+
+    <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 
+    <!-- Client-Side Validation Script -->
     <script type="text/javascript">
       function valid() {
         if (document.chngpwd.newpassword.value != document.chngpwd.confirmpassword.value) {
@@ -109,21 +153,24 @@ if (strlen($_SESSION['login']) == 0) {
         -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
         box-shadow: 0 1px 1px 0 rgba(0, 0, 0, .1);
       }
+
+      div {
+        font-family: Play;
+        font-size: 20px;
+        color: BLUE;
+      }
     </style>
   </head>
 
   <body style="background-color:aqua;">
 
-    <!-- Start Switcher -->
+    <!-- Color Switcher -->
     <?php include('includes/colorswitcher.php'); ?>
-    <!-- /Switcher -->
 
-    <!--Header-->
+    <!-- Header -->
     <?php include('includes/header.php'); ?>
-    <!-- /Header -->
-    <!--Page Header-->
 
-
+    <!-- Page Title -->
     <center>
       <div class="page-heading">
         <br /><br />
@@ -131,10 +178,8 @@ if (strlen($_SESSION['login']) == 0) {
       </div>
     </center>
 
-
-
-
     <?php
+    // Fetch user profile info for sidebar display
     $useremail = $_SESSION['login'];
     $sql = "SELECT * from tblusers where EmailId=:useremail";
     $query = $dbh->prepare($sql);
@@ -142,92 +187,97 @@ if (strlen($_SESSION['login']) == 0) {
     $query->execute();
     $results = $query->fetchAll(PDO::FETCH_OBJ);
     $cnt = 1;
+
     if ($query->rowCount() > 0) {
       foreach ($results as $result) { ?>
 
         <div class="container">
+          <!-- Profile Header Info -->
           <div class="user_profile_info gray-bg padding_4x4_40" style="background-color:aqua;">
-            <div class="upload_user_logo"> <img src="assets\images\dealer-logo.png" alt="image">
+            <div class="upload_user_logo">
+              <img src="assets\images\dealer-logo.png" alt="image">
             </div>
-
             <div class="dealer_info">
               <h5><?php echo htmlentities($result->FullName); ?></h5>
-              <p><?php echo htmlentities($result->Address); ?><br>
-                <?php echo htmlentities($result->City); ?>&nbsp;<?php echo htmlentities($result->Country);
-      }
-    } ?></p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-3 col-sm-3">
-          <?php include('includes/sidebar.php'); ?>
-          <div class="col-md-6 col-sm-8" style="background-color:black;">
-            <div class="profile_wrap">
-              <form name="chngpwd" method="post" onSubmit="return valid();">
-
-
-                <center>
-                  <h5 class="uppercase underline" style="background-color:blue;">MY PASSWORD</h5>
-                </center>
-
-                <?php if ($error) { ?>
-                  <div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } else if ($msg) { ?>
-                    <div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php } ?>
-                <div class="form-group">
-                  <label class="control-label" style="color:blue;">> CURRENT PASSWORD</label>
-                  <input class="form-control white_bg" id="password" name="password" type="password"
-                    placeholder="Current Password" required>
-                </div>
-                <div cl <div class="form-group">
-                  <label class="control-label" style="color:blue;">> NEW PASSWORD</label>
-                  <input class="form-control white_bg" id="newpassword" type="password" name="newpassword"
-                    placeholder="New Password" required>
-                </div>
-                <div class="form-group">
-                  <label class="control-label" style="color:blue;">> CONFIRM NEW PASSWORD</label>
-                  <input class="form-control white_bg" id="confirmpassword" type="password" name="confirmpassword"
-                    placeholder="Confirm New Password" required>
-                </div>
-
-                <div class="form-group">
-                  <input type="submit" value="UPDATE" name="update" id="submit" class="btn btn-block"
-                    style="background-color:blue;">
-                </div>
-              </form>
+              <p>
+                <?php echo htmlentities($result->Address); ?><br>
+                <?php echo htmlentities($result->City); ?>&nbsp;<?php echo htmlentities($result->Country); ?>
+              </p>
             </div>
           </div>
         </div>
+      <?php }
+    } ?>
+
+    <div class="row">
+      <!-- Sidebar -->
+      <div class="col-md-3 col-sm-3">
+        <?php include('includes/sidebar.php'); ?>
+
+        <!-- Password Form -->
+        <div class="col-md-6 col-sm-8" style="background-color:black;">
+          <div class="profile_wrap">
+            <form name="chngpwd" method="post" onSubmit="return valid();">
+
+              <center>
+                <h5 class="uppercase underline" style="background-color:blue;">MY PASSWORD</h5>
+              </center>
+
+              <!-- Notifications -->
+              <?php if ($error) { ?>
+                <div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div>
+              <?php } else if ($msg) { ?>
+                  <div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div>
+              <?php } ?>
+
+              <!-- Input Fields -->
+              <div class="form-group">
+                <label class="control-label" style="color:blue;">> CURRENT PASSWORD</label>
+                <input class="form-control white_bg" id="password" name="password" type="password"
+                  placeholder="Current Password" required>
+              </div>
+
+              <div class="form-group">
+                <label class="control-label" style="color:blue;">> NEW PASSWORD</label>
+                <input class="form-control white_bg" id="newpassword" type="password" name="newpassword"
+                  placeholder="New Password" required>
+              </div>
+
+              <div class="form-group">
+                <label class="control-label" style="color:blue;">> CONFIRM NEW PASSWORD</label>
+                <input class="form-control white_bg" id="confirmpassword" type="password" name="confirmpassword"
+                  placeholder="Confirm New Password" required>
+              </div>
+
+              <!-- Submit Button -->
+              <div class="form-group">
+                <input type="submit" value="UPDATE" name="update" id="submit" class="btn btn-block"
+                  style="background-color:blue;">
+              </div>
+
+            </form>
+          </div>
+        </div>
       </div>
-      <br /><br />
-      <!--/Profile-setting-->
+    </div>
 
-      <<!--Footer -->
-        <?php include('includes/footer.php'); ?>
-        <!-- /Footer-->
+    <br /><br />
 
+    <!-- Footer -->
+    <?php include('includes/footer.php'); ?>
 
-        <!--Login-Form -->
-        <?php include('includes/login.php'); ?>
-        <!--/Login-Form -->
+    <!-- Modals -->
+    <?php include('includes/login.php'); ?>
+    <?php include('includes/registration.php'); ?>
+    <?php include('includes/forgotpassword.php'); ?>
 
-        <!--Register-Form -->
-        <?php include('includes/registration.php'); ?>
-
-        <!--/Register-Form -->
-
-        <!--Forgot-password-Form -->
-        <?php include('includes/forgotpassword.php'); ?>
-        <!--/Forgot-password-Form -->
-
-        <!-- Scripts -->
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script src="assets/js/interface.js"></script>
-        <!--bootstrap-slider-JS-->
-        <script src="assets/js/bootstrap-slider.min.js"></script>
-        <!--Slider-JS-->
-        <script src="assets/js/slick.min.js"></script>
-        <script src="assets/js/owl.carousel.min.js"></script>
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/interface.js"></script>
+    <script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
 
   </body>
 
