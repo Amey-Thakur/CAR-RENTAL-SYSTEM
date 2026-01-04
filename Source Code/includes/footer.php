64de25101e30d024cf11cd1e7ac78a8ce9d3ca20
Amey Thakur <ameythakur20@gmail.com>
2026-01-04 17:36:07 -0500

    Car Rental Database Management System

--- Source Code/includes/footer.php
+++ Source Code/includes/footer.php
@@ -1,30 +1,66 @@
 <?php
 /**
- * Car Rental Database Management System
+ * ============================================================================
+ * Car Rental Database Management System - Page Footer
+ * ============================================================================
  * 
- * @author      Amey Thakur
+ * This file renders the global footer section of the website. It includes the
+ * navigation links, social media connections (if enabled), copyright information,
+ * and the newsletter subscription functionality.
+ * 
+ * ----------------------------------------------------------------------------
+ * AUTHORSHIP & CREDITS (AHNA Team)
+ * ----------------------------------------------------------------------------
+ * This project was developed by the AHNA team:
+ * - Amey Thakur
+ * - Hasan Rizvi
+ * - Nithya Gnanasekar
+ * - Anisha Gupta
+ * 
+ * @package     CarRentalSystem
+ * @subpackage  TemplateParts
+ * @author      Amey Thakur (Lead)
  * @link        https://github.com/Amey-Thakur
  * @repository  https://github.com/Amey-Thakur/CAR-RENTAL-SYSTEM
+ * @version     1.0.0
  * @date        2021-01-19
  * @license     MIT
+ * 
+ * ============================================================================
+ * CHANGE LOG:
+ * ----------------------------------------------------------------------------
+ * 2021-01-19 - Initial release - AHNA Team
+ * ============================================================================
  */
 
+/**
+ * Newsletter Subscription Handler
+ * 
+ * Processes form submission for the newsletter.
+ * Checks if the email already exists in the 'tblsubscribers' table to prevent
+ * duplicates before inserting.
+ */
 if (isset($_POST['emailsubscibe'])) {
   $subscriberemail = $_POST['subscriberemail'];
+
+  // Check for existing subscription
   $sql = "SELECT SubscriberEmail FROM tblsubscribers WHERE SubscriberEmail=:subscriberemail";
   $query = $dbh->prepare($sql);
   $query->bindParam(':subscriberemail', $subscriberemail, PDO::PARAM_STR);
   $query->execute();
   $results = $query->fetchAll(PDO::FETCH_OBJ);
   $cnt = 1;
+
   if ($query->rowCount() > 0) {
     echo "<script>alert('Already Subscribed.');</script>";
   } else {
+    // Insert new subscriber
     $sql = "INSERT INTO  tblsubscribers(SubscriberEmail) VALUES(:subscriberemail)";
     $query = $dbh->prepare($sql);
     $query->bindParam(':subscriberemail', $subscriberemail, PDO::PARAM_STR);
     $query->execute();
     $lastInsertId = $dbh->lastInsertId();
+
     if ($lastInsertId) {
       echo "<script>alert('Subscribed successfully.');</script>";
     } else {
@@ -38,6 +74,8 @@ if (isset($_POST['emailsubscibe'])) {
   <div class="footer-top">
     <div class="container">
       <div class="row">
+
+        <!-- About Us & Navigation Links -->
         <div class="col-md-6">
           <h6>About Us</h6>
           <ul>
@@ -47,6 +85,8 @@ if (isset($_POST['emailsubscibe'])) {
             <li><a href="page.php?type=aboutus">ABOUT US</a></li>
           </ul>
         </div>
+
+        <!-- Newsletter Subscription Form -->
         <div class="col-md-3 col-sm-6">
           <h6>Let's Keep in Touch</h6>
           <div class="newsletter-form">
@@ -60,13 +100,17 @@ if (isset($_POST['emailsubscibe'])) {
             </form>
           </div>
         </div>
+
       </div>
     </div>
   </div>
+
+  <!-- Copyright Section -->
   <div class="footer-bottom" style="background-color:blue;">
     <div class="container">
       <div class="row">
         <div class="col-md-6 col-md-push-6 text-right">
+          <!-- Social icons or additional links could go here -->
         </div>
         <div class="col-md-6 col-md-pull-6">
           <center>
