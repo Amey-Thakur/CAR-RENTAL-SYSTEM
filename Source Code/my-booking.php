ffe6b528d2d8e6b5fed40a0543a3dcec72d2e787
Amey Thakur <ameythakur20@gmail.com>
2026-01-04 17:31:10 -0500

    Car Rental Database Management System

--- Source Code/my-booking.php
+++ Source Code/my-booking.php
@@ -1,21 +1,68 @@
 <?php
 /**
- * Car Rental Database Management System
+ * ============================================================================
+ * Car Rental Database Management System - User Bookings
+ * ============================================================================
  * 
- * @author      Amey Thakur
+ * This file displays a private dashboard for the logged-in user to view their
+ * Booking History. It retrieves booking records joined with vehicle and brand
+ * information, and displays the status of each reservation (Confirmed, 
+ * Cancelled, or Pending).
+ * 
+ * ----------------------------------------------------------------------------
+ * AUTHORSHIP & CREDITS (AHNA Team)
+ * ----------------------------------------------------------------------------
+ * This project was developed by the AHNA team:
+ * - Amey Thakur
+ * - Hasan Rizvi
+ * - Nithya Gnanasekar
+ * - Anisha Gupta
+ * 
+ * @package     CarRentalSystem
+ * @subpackage  UserDashboard
+ * @author      Amey Thakur (Lead)
  * @link        https://github.com/Amey-Thakur
  * @repository  https://github.com/Amey-Thakur/CAR-RENTAL-SYSTEM
+ * @version     1.0.0
  * @date        2021-01-19
  * @license     MIT
+ * 
+ * @requires    PHP 7.0+
+ * @requires    MySQL 5.7+
+ * 
+ * ============================================================================
+ * CHANGE LOG:
+ * ----------------------------------------------------------------------------
+ * 2021-01-19 - Initial release - AHNA Team
+ * ============================================================================
  */
 
+/**
+ * Session Initialization
+ */
 session_start();
+
+/**
+ * Error Reporting
+ */
 error_reporting(0);
+
+/**
+ * Database Connection
+ */
 include('includes/config.php');
+
+/**
+ * Access Control Check
+ * 
+ * Verifies if the user is logged in by checking the 'login' session variable.
+ * If not logged in, redirects to the login page (index.php).
+ */
 if (strlen($_SESSION['login']) == 0) {
   header('location:index.php');
 } else {
-  ?><!DOCTYPE HTML>
+  ?>
+  <!DOCTYPE HTML>
   <html lang="en">
 
   <head>
@@ -24,21 +71,19 @@ if (strlen($_SESSION['login']) == 0) {
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="keywords" content="">
     <meta name="description" content="">
+
     <title>AHNA | CAR Rental</title>
-    <!--Bootstrap -->
+
+    <!-- CSS Dependencies -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
-    <!--Custome Style -->
     <link rel="stylesheet" href="assets/css/style.css" type="text/css">
-    <!--OWL Carousel slider-->
     <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
     <link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
-    <!--slick-slider -->
     <link href="assets/css/slick.css" rel="stylesheet">
-    <!--bootstrap-slider -->
     <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
-    <!--FontAwesome Font Style -->
     <link href="assets/css/font-awesome.min.css" rel="stylesheet">
-    <!-- Fav and touch icons -->
+
+    <!-- Browser Icons -->
     <link rel="apple-touch-icon-precomposed" sizes="144x144"
       href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114"
@@ -47,15 +92,17 @@ if (strlen($_SESSION['login']) == 0) {
       href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
     <link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
-    <!-- Google-Font-->
+
+    <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
   </head>
 
   <body style="background-color:aqua;">
-    <!--Header-->
-    <?php include('includes/header.php'); ?>
-    <!-- /Header -->
 
+    <!-- Header -->
+    <?php include('includes/header.php'); ?>
+
+    <!-- Page Title -->
     <center>
       <div class="page-heading">
         <br /><br />
@@ -64,6 +111,7 @@ if (strlen($_SESSION['login']) == 0) {
     </center>
 
     <?php
+    // Fetch User Profile Data for Display
     $useremail = $_SESSION['login'];
     $sql = "SELECT * from tblusers where EmailId=:useremail";
     $query = $dbh->prepare($sql);
@@ -71,91 +119,122 @@ if (strlen($_SESSION['login']) == 0) {
     $query->execute();
     $results = $query->fetchAll(PDO::FETCH_OBJ);
     $cnt = 1;
+
     if ($query->rowCount() > 0) {
       foreach ($results as $result) { ?>
 
+        <!-- User Profile Header -->
         <div class="container">
           <div class="user_profile_info gray-bg padding_4x4_40" style="background-color:aqua;">
-            <div class="upload_user_logo"> <img src="assets\images\dealer-logo.png" alt="image">
+            <div class="upload_user_logo">
+              <img src="assets\images\dealer-logo.png" alt="image">
             </div>
             <div class="dealer_info">
               <h5><?php echo htmlentities($result->FullName); ?></h5>
-              <p><?php echo htmlentities($result->Address); ?><br>
-                <?php echo htmlentities($result->City); ?>&nbsp;<?php echo htmlentities($result->Country);
-      }
-    } ?></p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-3 col-sm-3">
-          <?php include('includes/sidebar.php'); ?>
-          <div class="col-md-6 col-sm-8" style="background-color:black;">
-            <div class="profile_wrap">
-              <center>
-                <h5 class="uppercase underline" style="background-color:blue;">MY BOOKING</h5>
-              </center>
-
-              <ul class="vehicle_listing">
-                <?php
-                $useremail = $_SESSION['login'];
-                $sql = "SELECT tblvehicles.Vimage1 as Vimage1,tblvehicles.VehiclesTitle,tblvehicles.id as vid,tblbrands.BrandName,tblbooking.FromDate,tblbooking.ToDate,tblbooking.message,tblbooking.Status  from tblbooking join tblvehicles on tblbooking.VehicleId=tblvehicles.id join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand where tblbooking.userEmail=:useremail";
-                $query = $dbh->prepare($sql);
-                $query->bindParam(':useremail', $useremail, PDO::PARAM_STR);
-                $query->execute();
-                $results = $query->fetchAll(PDO::FETCH_OBJ);
-                $cnt = 1;
-                if ($query->rowCount() > 0) {
-                  foreach ($results as $result) { ?>
-                    <li>
-                      <div class="vehicle_img"> <a
-                          href="vehicle-details.php?vhid=<?php echo htmlentities($result->vid); ?>""><img src="
-                          admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1); ?>" alt="image"> </a> </div>
-                      <div class="vehicle_title">
-                        <h6><a href="vehicle-details.php?vhid=<?php echo htmlentities($result->vid); ?>"" style="
-                            color:white;"> <?php echo htmlentities($result->BrandName); ?> ,
-                            <?php echo htmlentities($result->VehiclesTitle); ?></a></h6>
-                        <p style="color:white;"><b>From Date:</b> <?php echo htmlentities($result->FromDate); ?><br /> <b>To
-                            Date:</b> <?php echo htmlentities($result->ToDate); ?></p>
-                      </div>
-                      <?php if ($result->Status == 1) { ?>
-                        <div class="vehicle_status"> <a href="#" class="btn outline btn-xs active-btn"
-                            style="background-color:blue;">CONFIRMED</a>
-                          <div class="clearfix"></div>
-                        </div>
-                      <?php } else if ($result->Status == 2) { ?>
-                          <div class="vehicle_status"> <a href="#" class="btn outline btn-xs"
-                              style="background-color:blue;">CANCELLED</a>
-                            <div class="clearfix"></div>
-                          </div>
-                      <?php } else { ?>
-                          <div class="vehicle_status" style="color:white;"> <a href="#" class="btn outline btn-xs"
-                              style="background-color:blue;">Not Confirmed</a>
-                            <div class="clearfix"></div>
-                          </div>
-                      <?php } ?>
-                      <div style="float: left" style="color:white;">
-                        <p style="color:white;"><b>Message:</b> <?php echo htmlentities($result->message); ?> </p>
-                      </div>
-                    </li>
-                  <?php }
-                } ?>
-              </ul>
-
+              <p>
+                <?php echo htmlentities($result->Address); ?><br>
+                <?php echo htmlentities($result->City); ?>&nbsp;<?php echo htmlentities($result->Country); ?>
+              </p>
             </div>
           </div>
         </div>
+      <?php }
+    } ?>
+
+    <div class="row">
+      <!-- Sidebar Navigation -->
+      <div class="col-md-3 col-sm-3">
+        <?php include('includes/sidebar.php'); ?>
       </div>
-      <br /><br />
-      <?php include('includes/footer.php'); ?>
-      <!-- Scripts -->
-      <script src="assets/js/jquery.min.js"></script>
-      <script src="assets/js/bootstrap.min.js"></script>
-      <script src="assets/js/interface.js"></script>
-      <!--bootstrap-slider-JS-->
-      <script src="assets/js/bootstrap-slider.min.js"></script>
-      <!--Slider-JS-->
-      <script src="assets/js/slick.min.js"></script>
-      <script src="assets/js/owl.carousel.min.js"></script>
+
+      <!-- Booking List Content -->
+      <div class="col-md-6 col-sm-8" style="background-color:black;">
+        <div class="profile_wrap">
+          <center>
+            <h5 class="uppercase underline" style="background-color:blue;">MY BOOKING</h5>
+          </center>
+
+          <ul class="vehicle_listing">
+            <?php
+            // Fetch User Bookings with Vehicle and Brand details
+            $useremail = $_SESSION['login'];
+            $sql = "SELECT tblvehicles.Vimage1 as Vimage1,tblvehicles.VehiclesTitle,tblvehicles.id as vid,tblbrands.BrandName,tblbooking.FromDate,tblbooking.ToDate,tblbooking.message,tblbooking.Status  from tblbooking join tblvehicles on tblbooking.VehicleId=tblvehicles.id join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand where tblbooking.userEmail=:useremail";
+            $query = $dbh->prepare($sql);
+            $query->bindParam(':useremail', $useremail, PDO::PARAM_STR);
+            $query->execute();
+            $results = $query->fetchAll(PDO::FETCH_OBJ);
+            $cnt = 1;
+
+            if ($query->rowCount() > 0) {
+              foreach ($results as $result) { ?>
+                <!-- Booking Item -->
+                <li>
+                  <!-- Vehicle Image -->
+                  <div class="vehicle_img">
+                    <a href="vehicle-details.php?vhid=<?php echo htmlentities($result->vid); ?>">
+                      <img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1); ?>" alt="image">
+                    </a>
+                  </div>
+
+                  <!-- Booking Details -->
+                  <div class="vehicle_title">
+                    <h6>
+                      <a href="vehicle-details.php?vhid=<?php echo htmlentities($result->vid); ?>" style="color:white;">
+                        <?php echo htmlentities($result->BrandName); ?> , <?php echo htmlentities($result->VehiclesTitle); ?>
+                      </a>
+                    </h6>
+                    <p style="color:white;">
+                      <b>From Date:</b> <?php echo htmlentities($result->FromDate); ?><br />
+                      <b>To Date:</b> <?php echo htmlentities($result->ToDate); ?>
+                    </p>
+                  </div>
+
+                  <!-- Booking Status Logic -->
+                  <?php if ($result->Status == 1) { ?>
+                    <!-- Confirmed -->
+                    <div class="vehicle_status">
+                      <a href="#" class="btn outline btn-xs active-btn" style="background-color:blue;">CONFIRMED</a>
+                      <div class="clearfix"></div>
+                    </div>
+                  <?php } else if ($result->Status == 2) { ?>
+                      <!-- Cancelled -->
+                      <div class="vehicle_status">
+                        <a href="#" class="btn outline btn-xs" style="background-color:blue;">CANCELLED</a>
+                        <div class="clearfix"></div>
+                      </div>
+                  <?php } else { ?>
+                      <!-- Pending/Not Confirmed -->
+                      <div class="vehicle_status" style="color:white;">
+                        <a href="#" class="btn outline btn-xs" style="background-color:blue;">Not Confirmed</a>
+                        <div class="clearfix"></div>
+                      </div>
+                  <?php } ?>
+
+                  <!-- User Message -->
+                  <div style="float: left; color:white;">
+                    <p style="color:white;"><b>Message:</b> <?php echo htmlentities($result->message); ?> </p>
+                  </div>
+                </li>
+              <?php }
+            } ?>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+    <br /><br />
+
+    <!-- Footer -->
+    <?php include('includes/footer.php'); ?>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/interface.js"></script>
+    <script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
   </body>
 
   </html>
