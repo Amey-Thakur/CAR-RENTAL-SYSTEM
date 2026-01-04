f9df12e607f7d6a4fd15221612312335c53d22d8
Amey Thakur <ameythakur20@gmail.com>
2026-01-04 17:10:14 -0500

    Car Rental Database Management System

--- Source Code/admin/includes/leftbar.php
+++ Source Code/admin/includes/leftbar.php
@@ -1,31 +1,40 @@
-	<nav class="ts-sidebar">
-			<ul class="ts-sidebar-menu">
-			
-				<li class="ts-label">Main</li>
-				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-			
-<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
-<ul>
-<li><a href="create-brand.php">Create Brand</a></li>
-<li><a href="manage-brands.php">Manage Brands</a></li>
-</ul>
-</li>
+<!--
+ * Car Rental Database Management System - Admin Module
+ * 
+ * @author      Amey Thakur
+ * @link        https://github.com/Amey-Thakur
+ * @repository  https://github.com/Amey-Thakur/CAR-RENTAL-SYSTEM
+ * @date        2021-01-19
+ * @license     MIT
+-->
+<nav class="ts-sidebar">
+	<ul class="ts-sidebar-menu">
 
-<li><a href="#"><i class="fa fa-sitemap"></i> Vehicles</a>
-					<ul>
-						<li><a href="post-avehicle.php">Post a Vehicle</a></li>
-						<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
-					</ul>
-				</li>
-				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+		<li class="ts-label">Main</li>
+		<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
 
-				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
-				<li><a href="manage-contactusquery.php"><i class="fa fa-desktop""></i> Manage Contactus Query</a></li>
-				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
-			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
-			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
-
-			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li>
-
+		<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
+			<ul>
+				<li><a href="create-brand.php">Create Brand</a></li>
+				<li><a href="manage-brands.php">Manage Brands</a></li>
 			</ul>
-		</nav>
+		</li>
+
+		<li><a href="#"><i class="fa fa-sitemap"></i> Vehicles</a>
+			<ul>
+				<li><a href="post-avehicle.php">Post a Vehicle</a></li>
+				<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+			</ul>
+		</li>
+		<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+
+		<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
+		<li><a href="manage-contactusquery.php"><i class="fa fa-desktop""></i> Manage Contactus Query</a></li>
+				<li><a href=" reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+		<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
+		<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+
+		<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li>
+
+	</ul>
+</nav>
--- Source Code/admin/logout.php
+++ Source Code/admin/logout.php
@@ -1,15 +1,29 @@
 <?php
-session_start(); 
+/**
+ * Car Rental Database Management System - Admin Module
+ * 
+ * @author      Amey Thakur
+ * @link        https://github.com/Amey-Thakur
+ * @repository  https://github.com/Amey-Thakur/CAR-RENTAL-SYSTEM
+ * @date        2021-01-19
+ * @license     MIT
+ */
+
+session_start();
 $_SESSION = array();
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 60*60,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
+    setcookie(
+        session_name(),
+        '',
+        time() - 60 * 60,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
     );
 }
 unset($_SESSION['login']);
 session_destroy(); // destroy session
-header("location:index.php"); 
+header("location:index.php");
 ?>
-
