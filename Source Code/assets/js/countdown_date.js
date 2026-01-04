b97f03a0fdc1c58d111742eba64537fa51800c91
Amey Thakur <ameythakur20@gmail.com>
2026-01-04 18:06:24 -0500

    Car Rental Database Management System

--- Source Code/assets/js/countdown_date.js
+++ Source Code/assets/js/countdown_date.js
@@ -1,11 +1,49 @@
+/**
+ * ============================================================================
+ * Car Rental Database Management System - Countdown Initialization
+ * ============================================================================
+ * 
+ * This script initializes the countdown timer used on special offer pages
+ * or upcoming vehicle launch announcements. It uses the `jquery.countdown`
+ * plugin to render a live countdown to a specified target date.
+ * 
+ * ----------------------------------------------------------------------------
+ * AUTHORSHIP & CREDITS (AHNA Team)
+ * ----------------------------------------------------------------------------
+ * This project was developed by the AHNA team:
+ * - Amey Thakur
+ * - Hasan Rizvi
+ * - Nithya Gnanasekar
+ * - Anisha Gupta
+ * 
+ * @package     CarRentalSystem
+ * @subpackage  Assets/JS
+ * @author      Amey Thakur (Lead)
+ * @link        https://github.com/Amey-Thakur
+ * @repository  https://github.com/Amey-Thakur/CAR-RENTAL-SYSTEM
+ * @version     1.0.0
+ * @date        2021-01-19
+ * @license     MIT
+ * 
+ * ============================================================================
+ * CHANGE LOG:
+ * ----------------------------------------------------------------------------
+ * 2021-01-19 - Initial release - AHNA Team
+ * ============================================================================
+ */
+
 /*------------------------------------------------------------------
-	Countdown
-	-------------------------------------------------------------------*/
- 
-	var endDate = "march 22, 2017";
-	$('.countdown').countdown({
-	  date: endDate,
-	  render: function(data) {
+	Countdown Initialization
+-------------------------------------------------------------------*/
+
+// Target date for the countdown
+var endDate = "march 22, 2017";
+
+// Initialize the countdown plugin on elements with class 'countdown'
+$('.countdown').countdown({
+	date: endDate,
+	render: function (data) {
+		// Custom HTML rendering for the countdown display
 		$(this.el).html("<div class='countdown-amount'>" + this.leadingZeros(data.days, 2) + " <span class='countdown-period'>Days</span></div><div class='countdown-amount'>" + this.leadingZeros(data.hours, 2) + " <span class='countdown-period'>Hours</span></div><div class='countdown-amount'>" + this.leadingZeros(data.min, 2) + " <span class='countdown-period'>Minutes</span></div><div class='countdown-amount'>" + this.leadingZeros(data.sec, 2) + " <span class='countdown-period'>Seconds</span></div>");
-	  }
-	});	
+	}
+});
